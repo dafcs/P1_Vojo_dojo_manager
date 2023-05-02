089389eecf7e2ae34f57495736555297c225d773
Daniel FCS <danielf.cs@outlook.com>
2023-05-02 14:57:43 +0100

    removed date and time from lesson:

--- models/lesson.py
+++ models/lesson.py
@@ -1,6 +1,4 @@
 class Lesson():
-    def __init__(self,name,date,time,id = None):
+    def __init__(self,name, id = None):
         self.name = name
-        self.date = date
-        self.time = time
         self.id = id
